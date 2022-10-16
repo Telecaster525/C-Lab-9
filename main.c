3b2866b0fc4f51dc172bdd1b0de289d28bdce0af
Said Gadzhiev <94011547+Telecaster525@users.noreply.github.com>
2022-10-16 11:02:06 +0300

    Update main.c

--- main.c
+++ main.c
@@ -17,8 +17,29 @@ int main() {
     }
     printf("Number of lowercase letters in a string: %d\n", count_small);
     printf("Number of uppercase letters in a string: %d\n", count_big);
-    printf("Number of digits in a string: %d", count_numbers);
-    printf("#Task 5#");
+    printf("Number of digits in a string: %d\n", count_numbers);
 
+    printf("#Task 2#\n");
+    int minutes, pay, overpay;
+
+    printf("Enter the cost of a monthly package (499 minutes): ");
+    scanf("%d", &pay);
+    printf("\n");
+
+    printf("Enter the number of minutes spent: ");
+    scanf("%d", &minutes);
+    printf("\n");
+
+    printf("Enter the cost of extra minutes included in the package: ");
+    scanf("%d", &overpay);
+    printf("\n");
+
+    int pereplata = pay + (minutes - 499) * overpay;
+    if (minutes <= 499) {
+        printf("Service package cost: %d rubles", pay);
+    }
+    else if (minutes > 499) {
+        printf("Service package cost: %d rubles", pereplata);
+    }
     return 0;
 }
